f8bfb1638de33d46491e778d251fadf570c3e64d
Administrator <Administrator@CHETUIWK884>
2022-03-11 17:13:50 +0530

    Add project files.

---
+++ WebApplication chandan/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication_chandan.Models;
+
+namespace WebApplication_chandan.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly EmployeeDbContext db;
+        public EmployeeController(EmployeeDbContext context)
+        {
+            db = context;
+
+        }
+        public IActionResult create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult create(Employee abc)
+        {
+            db.Employees.Add(abc);
+            db.SaveChanges();
+            return RedirectToAction ("Indext");
+        }
+    }
+}
---
+++ WebApplication chandan/Models/EmployeeDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication_chandan.Models
+{
+    public class EmployeeDbContext:DbContext
+    {
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> Options) : base(Options) { }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
